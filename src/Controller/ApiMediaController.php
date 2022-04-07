5f7c4fc56cf7f314de288fda5bd56e8ae29073d4
ROBERT Benjamin <emailpourfb90@gmail.com>
2022-04-07 10:34:11 +0200

    fix media download

--- src/Controller/ApiMediaController.php
+++ src/Controller/ApiMediaController.php
@@ -116,7 +116,7 @@ class ApiMediaController extends AbstractController
         $format = null
     ): RedirectResponse
     {
-        if ($format) {
+        if ($format && strpos('image', $media->getMimeType()) !== false) {
             $path = $this->mediaService->getImagePath($media, $format);
         } else {
             $path = $this->mediaService->getMediaPath($media);
@@ -143,7 +143,7 @@ class ApiMediaController extends AbstractController
               $format = null
     ): RedirectResponse
     {
-        if ($format) {
+        if ($format && strpos('image', $media->getMimeType()) !== false) {
             $path = $this->mediaService->getImagePath($media, $format);
         } else {
             $path = $this->mediaService->getMediaPath($media);
