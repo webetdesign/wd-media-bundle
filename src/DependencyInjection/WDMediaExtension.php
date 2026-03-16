2ea1fc6289e300deef1b8769a3aa399ec5000f02
Clement <clement@webetdesign.com>
2026-03-16 14:27:18 +0100

    Refactor and enhance type definitions across bundles
    Updated method signatures to include stricter type hints and return types. Replaced deprecated Symfony `HttpKernel\DependencyInjection\Extension` with `DependencyInjection\Extension\Extension`. Introduced optional `Vote` parameter in Voter classes to align with newer Symfony API features.

--- src/DependencyInjection/WDMediaExtension.php
+++ src/DependencyInjection/WDMediaExtension.php
@@ -4,9 +4,8 @@ namespace WebEtDesign\MediaBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use WebEtDesign\RgpdBundle\Annotations\Exportable;
 
 /**
  * This is the class that loads and manages your bundle configuration.
